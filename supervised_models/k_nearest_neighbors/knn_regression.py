04b3d6ea26e99c9820e4db1e4e244f4468bf2b89
Juan David Briceno Guerrero <62699983+juandavidbriceno@users.noreply.github.com>
2021-03-25 08:59:47 +0100

    Update knn_regression.py

--- supervised_models/k_nearest_neighbors/knn_regression.py
+++ supervised_models/k_nearest_neighbors/knn_regression.py
@@ -30,7 +30,7 @@ X_train, X_test, y_train, y_test = train_test_split(X_R1, y_R1, random_state = 0
 
 #4.Training a generic model.
 #There is only one feature and for that reason there is not scaling part in this example.
-knnreg = KNeighborsRegressor(n_neighbors = 5).fit(X_train, y_train)
+knnreg = KNeighborsRegressor(n_neighbors = 2).fit(X_train, y_train)
 print('R-squared training score: {:.3f}'
      .format(knnreg.score(X_train, y_train)))
 print('R-squared test score: {:.3f}'
