4d457f8057975aaf98da66dab0a7df4a2c96200f
Juan David Briceno Guerrero <62699983+juandavidbriceno@users.noreply.github.com>
2021-03-25 09:27:59 +0100

    Update logistic_regression_binary_classification.py

--- supervised_models/logistic_regression/logistic_regression_binary_classification.py
+++ supervised_models/logistic_regression/logistic_regression_binary_classification.py
@@ -13,6 +13,7 @@ from adspy_shared_utilities import plot_class_regions_for_classifier_subplot
 import seaborn as sn
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import GridSearchCV
 
 #2.Import the dataset of fruits.
 fruits = pd.read_csv('/home/juan-david/Documents/data_science/travail_personnel/machine_learning_michigan_university/fruit_data_with_colors.txt',delimiter = "\t")
@@ -45,25 +46,31 @@ print('train score of the model :'+str(clf.score(X_train,y_train)))
 print('test score of the model :'+str(clf.score(X_test,y_test)))
 
 #7.Tuning the model.
-results_train = []
-results_test = []
-classifiers = []
+#defining the param grid.
 
-for c in [0.001,0.01,0.1, 0.5, 1,50, 100]:
-    clf = LogisticRegression(C=c).fit(X_train, y_train)
-    val_train = clf.score(X_train, y_train)
-    val_test = clf.score(X_test, y_test)
-    results_train.append(val_train)
-    results_test.append(val_test)
-    classifiers.append(clf)
-        
-best_test_result = max(results_test)
-best_train_result = results_train[results_test.index(best_test_result)]
-best_classifier = classifiers[results_test.index(best_test_result)]
+logModel = LogisticRegression()
 
-print("Training set accuracy :"+ str(best_train_result))
-print("Test set accuracy :"+ str(best_test_result))
-print(best_classifier.get_params(deep=True))
+c = [0.001,0.01,0.1, 0.5, 1,50, 100]
+
+param_grid = [
+    {'penalty' : ['l1', 'l2', 'elasticnet','none'],
+    'C' : c,
+    'solver' : ['lbfgs', 'newton-cg', 'liblinear', 'sag', 'saga'],
+    'max_iter' : [100, 1000, 2500, 5000]}
+]
+
+clf = GridSearchCV(logModel, param_grid =param_grid, cv =3, verbose =True, n_jobs=1)
+clf.fit(X_train,y_train)
+
+best_train_result_1 = clf.score(X_train,y_train)
+best_test_result_1 = clf.score(X_test,y_test)
+best_classifier_1 = clf
+
+print("Training set accuracy :"+ str(best_train_result_1))
+print("Test set accuracy :"+ str(best_test_result_1))
+#print(best_classifier.get_params(deep=True))
+
+print(best_classifier_1.best_estimator_)
 
 
 #8.Applying logistic regression over the brest cancer data set.
@@ -94,22 +101,26 @@ print('Accuracy of Logistic regression classifier on test set: {:.2f}'
      .format(clf.score(X_test, y_test)))
 
 #9.Tuning the model.
-results_train = []
-results_test = []
-classifiers = []
+logModel = LogisticRegression()
 
-for c in [0.001,0.01,0.1,0.5, 1,50, 100]:
-    clf = LogisticRegression(C=c).fit(X_train, y_train)
-    val_train = clf.score(X_train, y_train)
-    val_test = clf.score(X_test, y_test)
-    results_train.append(val_train)
-    results_test.append(val_test)
-    classifiers.append(clf)
-        
-best_test_result = max(results_test)
-best_train_result = results_train[results_test.index(best_test_result)]
-best_classifier = classifiers[results_test.index(best_test_result)]
+c = [0.001,0.01,0.1, 0.5, 1,50, 100]
 
-print("Training set accuracy :"+ str(best_train_result))
-print("Test set accuracy :"+ str(best_test_result))
-print(best_classifier.get_params(deep=True))
+param_grid = [
+    {'penalty' : ['l1', 'l2', 'elasticnet','none'],
+    'C' : c,
+    'solver' : ['lbfgs', 'newton-cg', 'liblinear', 'sag', 'saga'],
+    'max_iter' : [100, 1000, 2500, 5000]}
+]
+
+clf = GridSearchCV(logModel, param_grid =param_grid, cv =3, verbose =True, n_jobs=1)
+clf.fit(X_train,y_train)
+
+best_train_result_2 = clf.score(X_train,y_train)
+best_test_result_2 = clf.score(X_test,y_test)
+best_classifier_2 = clf
+
+print("Training set accuracy :"+ str(best_train_result_2))
+print("Test set accuracy :"+ str(best_test_result_2))
+#print(best_classifier.get_params(deep=True))
+
+print(best_classifier_2.best_estimator_)
